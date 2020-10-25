c63988293ce027fdd51a9fe47201a668db09c5c8
anamika-99 <43959159+anamika-99@users.noreply.github.com>
2020-10-25 17:00:48 +0530

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("localhost","root","","parking");
+$conn=mysqli_connect("vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	","u5c6dgfgisl2jjid","hs8sigl6t16gqd33","to42z1r546dhx1a7");
 $sql = "CREATE TABLE users (
 id int(100) NOT NULL AUTO_INCREMENT primary Key , 
 slot  int(50),
