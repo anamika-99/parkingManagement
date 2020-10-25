a48dfab1451fc694905e787fafea873cba7e9044
anamika-99 <43959159+anamika-99@users.noreply.github.com>
2020-10-25 17:02:34 +0530

    Update db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,2 +1,2 @@
 <?php
-$conn=mysqli_connect("localhost","root","","parking");
+$conn=mysqli_connect("vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	","u5c6dgfgisl2jjid","hs8sigl6t16gqd33","to42z1r546dhx1a7");
